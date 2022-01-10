36d9b4a8dc4a7710b99b460415956f1c6023f9d3
Jason Herrera <baytownjason@yahoo.com>
2022-01-10 13:26:35 -0600

    completed everything from vid 1

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,20 +1,22 @@
 module Api
     module V1
-        class AirlineController < ApplicationController
+        class AirlinesController < ApplicationController
+            protect_from_forgery with: :null_session
+
             def index
                 airlines = Airline.all 
-                render json: AirlineSerializer.new(airlines, options).serialized_json
+                render json: AirlineSerializer.new(airlines, options).serializable_hash.to_json
             end
 
             def show
                 airline = Airline.find_by(slug: params[:slug])
-                render json: AirlineSerializer.new(airline, options).serialized_json
+                render json: AirlineSerializer.new(airline, options).serializable_hash.to_json
             end
 
             def create
                 airline = Airline.new(airline_params)
                 if airline.save
-                    render json: AirlineSerializer.new(airline).serialized_json
+                    render json: AirlineSerializer.new(airline).serializable_hash.to_json
                 else 
                     render json: { error: airline.errors.messages }, status: 422
                 end
@@ -23,7 +25,7 @@ module Api
             def update
                 airline = Airline.find_by(slug: params[:slug])
                 if airline.update(airline_params)
-                    render json: AirlineSerializer.new(airline, options).serialized_json
+                    render json: AirlineSerializer.new(airline, options).serializable_hash.to_json
                 else
                     render json: { error: airline.errors.messages }, status: 422
                 end
@@ -45,7 +47,7 @@ module Api
             end
 
             def options
-                @options ||* { include: %i[reviews] }
+                @options ||= { include: %i[reviews] }
             end
         end
     end
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,10 +1,12 @@
 module Api 
     module V1 
         class ReviewsController < ApplicationController
+            protect_from_forgery with: :null_session
+
             def create
                 review = Review.new(review_params)
                 if review.save
-                    render json: ReviewSerializer.new(review).serialized_json
+                    render json: ReviewSerializer.new(review).serializable_hash.to_json
                 else
                     render json: { error: review.errors.messages }, status: 422
                 end
